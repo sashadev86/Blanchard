e3d3b641fddfe52c4091f873a02825b32ee397ca
Aliaksandr <djvalentinoff@gmail.com>
2022-05-05 20:15:40 +0300

    fixed bug accordeon

--- js/accordion.js
+++ js/accordion.js
@@ -1,12 +1,15 @@
-$( function() {
-  $( ".accordion__list" ).accordion({
-     icons: false,
-     heightStyle: "content",
-     collapsible: true,
-     active: false,
-     animate: {
-       duration: 1000,
-       easing: 'easeInQuad'
-     }
-   });
- });
+$(function () {
+  $(".accordion__list").accordion({
+    icons: false,
+    heightStyle: "content",
+    collapsible: true,
+    active: 0,
+    animate: {
+      duration: 1000,
+      easing: 'easeInQuad'
+    },
+    create: function (event, ui) {
+      $(".accordion__heading").attr("tabIndex", "0");
+    },
+  });
+});

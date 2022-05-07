f9cbfad2b452a14a6f3cae1c60a4d26917486f26
Aliaksandr <djvalentinoff@gmail.com>
2022-05-07 12:24:25 +0300

    remade the accordion

--- js/accordion.js
+++ js/accordion.js
@@ -1,15 +1,5 @@
-$(function () {
-  $(".accordion__list").accordion({
-    icons: false,
-    heightStyle: "content",
-    collapsible: true,
-    active: 0,
-    animate: {
-      duration: 1000,
-      easing: 'easeInQuad'
-    },
-    create: function (event, ui) {
-      $(".accordion__heading").attr("tabIndex", "0");
-    },
+(() => {
+  new Accordion(".js-accordion-container", {
+    openOnInit: [0]
   });
-});
+})();
